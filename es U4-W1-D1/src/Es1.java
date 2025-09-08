bb0ffabde9a7eba545269541c619f99505c1f4c1
AminTabite <Aminepicode13@gmail.com>
2025-09-08 22:42:45 +0200

    Update Es1.java
    es 1 done

--- es U4-W1-D1/src/Es1.java
+++ es U4-W1-D1/src/Es1.java
@@ -1,8 +1,23 @@
+import java.util.Arrays;
+
 public class Es1 {
+
+    public static char[] elements = new char[6];
 
     public static void main(String[] args) {
         Multiply(6, 7);
         Toconcat("gatto", 4, 2).concat("prova");
+
+
+        elements[0] = 'A';
+        elements[1] = 'B';
+        elements[2] = 'C';
+        elements[3] = 'D';
+        elements[4] = 'E';
+
+        char random = 'Z';
+
+        AddintoArray(elements, random);
 
 
     }
@@ -14,11 +29,34 @@ public class Es1 {
         return n1 * n2;
     }
 
-
     public static String Toconcat(String str1, int n3, int n4) {
         System.out.println(" unione degli elementi" + " " + (str1 + String.valueOf(n3) + " " + String.valueOf(n4)));
         return str1 + String.valueOf(n3) + " " + String.valueOf(n4);
     }
 
+    public static char[] AddintoArray(char[] arr, char random) {
+
+        int i = 0;
+
+        char[] NewArray = new char[6];
+
+        {
+
+
+            NewArray[0] = elements[3];
+            NewArray[1] = elements[4];
+            NewArray[2] = random;
+            NewArray[3] = elements[0];
+            NewArray[4] = elements[1];
+            NewArray[5] = elements[2];
+
+            System.out.printf("Il nuovo array e'" + " " + Arrays.toString(NewArray));
+
+        }
+        ;
+        return NewArray;
+
+
+    }
 
 }
