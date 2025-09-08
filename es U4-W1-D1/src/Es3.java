868df43817101c961c96e010ec522b37cc404924
AminTabite <Aminepicode13@gmail.com>
2025-09-08 17:10:24 +0200

    almost done es 3
    .

--- es U4-W1-D1/src/Es3.java
+++ es U4-W1-D1/src/Es3.java
@@ -7,12 +7,27 @@ public class Es3 {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("inserire il primo lato ");
+       /*  System.out.println("inserire il primo lato "); // es 1
         double L1 = scanner.nextDouble();
         System.out.println("inserire il secondo lato ");
         double L2 = scanner.nextDouble();
 
-        double Perimetro = PerimetroRettangolo(L1, L2);
+        double Perimetro = PerimetroRettangolo(L1, L2);*/
+
+        System.out.println("Inserisci un numero");  //es 2
+        int N6 = scanner.nextInt();
+
+        int Result = IsN6EvenorOdd(N6);
+        System.out.println(Result);
+
+
+        System.out.println("Inserire il primo lato");
+        double lt1 = scanner.nextDouble();
+        System.out.println("Inserire il secondo lato");
+        double lt2 = scanner.nextDouble();
+
+        double Perimetro = PerimetroT(lt1, lt2);
+
     }
 
     public static double PerimetroRettangolo(double L1, double L2) {
@@ -21,5 +36,20 @@ public class Es3 {
         return 2 * (L1 + L2);
     }
 
+    public static int IsN6EvenorOdd(int N6) {
+
+        if (N6 % 2 == 0) {
+            return 0;
+        } else {
+            return 1;
+        }
+
+
+    }
+
+    public static double PerimetroT(double lt1, double lt2) {
+
+        
+    }
 
 }
