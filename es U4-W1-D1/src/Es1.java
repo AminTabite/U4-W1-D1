02be94df7c3fd0966709e47a0b9255f230d2884c
AminTabite <Aminepicode13@gmail.com>
2025-09-08 15:26:19 +0200

    es 1
    es 1 almost done

---
+++ es U4-W1-D1/src/Es1.java
@@ -0,0 +1,29 @@
+public class Es1 {
+
+    public static void main(String[] args) {
+        Multiply(6, 7);
+        Toconcat("gatto", 4, 2).concat("prova");
+
+
+    }
+
+    public static int Multiply(int n1, int n2) {
+
+        System.out.println("il risultato e' " + " " + (n1 * n2));
+
+        return n1 * n2;
+    }
+
+
+    public static String Toconcat(String str1, int n3, int n4) {
+        System.out.println(" unione degli elementi" + " " + (str1 + String.valueOf(n3) + " " + String.valueOf(n4)));
+        return str1 + String.valueOf(n3) + " " + String.valueOf(n4);
+    }
+
+
+    public static 
+
+
+
+
+}
