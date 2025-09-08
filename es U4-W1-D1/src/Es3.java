0aba3e4e93a183673853ea98a8be0fadaf86f288
AminTabite <Aminepicode13@gmail.com>
2025-09-08 16:39:02 +0200

    1st es 3

--- es U4-W1-D1/src/Es3.java
+++ es U4-W1-D1/src/Es3.java
@@ -1,2 +1,25 @@
+import java.util.Scanner;
+
 public class Es3 {
+
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("inserire il primo lato ");
+        double L1 = scanner.nextDouble();
+        System.out.println("inserire il secondo lato ");
+        double L2 = scanner.nextDouble();
+
+        double Perimetro = PerimetroRettangolo(L1, L2);
+    }
+
+    public static double PerimetroRettangolo(double L1, double L2) {
+
+        System.out.println("il perimetro e'" + " " + 2 * (L1 + L2) + "cm");
+        return 2 * (L1 + L2);
+    }
+
+
 }
